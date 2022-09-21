a3626ce9e7889945afce6efc1d25725e6bb9e3f9
jec-ode93 <jeanemmanuelcadet@gmail.com>
2022-09-21 01:38:34 -0400

    Add Key Points

--- bin/starter.dart
+++ bin/starter.dart
@@ -15,3 +15,10 @@ main() {
   print('Popped : $element'); // Popped : 4
   print('Popped : $el'); // Popped : 3
 }
+
+// Key Points
+// • A stack is a LIFO, last-in first-out, data structure.
+// • Despite being so simple, the stack is a key data structure for many problems.
+// • The only two essential operations for a stack are push for adding elements and
+// pop for removing elements.
+// • push and pop are both constant-time operations.
